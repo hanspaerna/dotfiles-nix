916df78b0ba2c86287e708402e5245d48e8cc082
Hans Paerna <sqlob@protonmail.ch>
2026-09-20 00:04:35 +0200

    automatically start steam in plasma

--- ally-nixos/home.nix
+++ ally-nixos/home.nix
@@ -9,18 +9,27 @@
     username = "yanumibaal";
     homeDirectory = "/home/yanumibaal";
 
-      # Add "Return to Gaming Mode" desktop shortcut, like in Steam Deck
-      file."Desktop/Return-to-Gaming-Mode.desktop".source =
-        (pkgs.makeDesktopItem {
-          desktopName = "Return to Gaming Mode";
-          exec = "qdbus org.kde.Shutdown /Shutdown org.kde.Shutdown.logout";
-          icon = "steam";
-          name = "Return-to-Gaming-Mode";
-          startupNotify = false;
-          terminal = false;
-          type = "Application";
-        })
-        + "/share/applications/Return-to-Gaming-Mode.desktop";
+    # Add "Return to Gaming Mode" desktop shortcut, like in Steam Deck
+    file."Desktop/Return-to-Gaming-Mode.desktop".source =
+      (pkgs.makeDesktopItem {
+        desktopName = "Return to Gaming Mode";
+        exec = "qdbus org.kde.Shutdown /Shutdown org.kde.Shutdown.logout";
+        icon = "steam";
+        name = "Return-to-Gaming-Mode";
+        startupNotify = false;
+        terminal = false;
+        type = "Application";
+      })
+      + "/share/applications/Return-to-Gaming-Mode.desktop";
+
+
+    file.".config/autostart/steam.desktop".text = ''
+      [Desktop Entry]
+      Type=Application
+      Name=Steam
+      Exec=${pkgs.steam}/bin/steam -silent
+      Terminal=false
+    '';
 
     stateVersion = "26.11";
   };
